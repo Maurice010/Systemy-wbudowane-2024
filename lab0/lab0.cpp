9a6b2915e2a4fe5fc0c401ae6cfce6abeb79a387
Jakub Kozak <85180405+Maurice010@users.noreply.github.com>
2024-03-05 21:39:33 +0100

    Add files via upload

---
+++ lab0/lab0.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <functional>
+
+using namespace std;
+
+bool gateNOT(bool value) {
+    if (value == true) return false;
+    else return true;
+}
+
+bool gateAND(bool value1, bool value2) {
+    if (value1 == true && value2 == true) return true;
+    else return false;
+}
+
+bool gateOR(bool value1, bool value2) {
+    if (value1 == false && value2 == false) return false;
+    else return true;
+}
+
+void table(const function<bool(bool, bool)>& fun, string name) {
+    cout << name << endl;
+    for (bool value1 : {false, true}) {
+        for (bool value2 : {false, true}) {
+            cout << value1 << " " << value2 << " " << fun(value1, value2) << endl;
+        }
+    }
+    cout << endl;
+}
+
+void table(const function<bool(bool)>& fun, string name) {
+    cout << name << endl;
+    for (bool value : {false, true}) {
+         cout << value << " " << fun(value) << endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    table(gateNOT, "NOT");
+    table(gateAND, "AND");
+    table(gateOR, "OR");
+}
